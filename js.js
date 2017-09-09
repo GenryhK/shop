5f9c1174c7b2bc36668333d7059f765646dcb7fb
Genryh Kovalenko <kovalenko.genryh@gmail.com>
2017-09-09 17:09:58 +0300

    add logo

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-let div = document.createElement(`div`);
+/*let div = document.createElement(`div`);
 div.textContent=`12323456789023456`;
 div.style.display=`none`
 let button=document.createElement(`button`);
@@ -6,4 +6,16 @@ button.textContent=`hide`;
 function hide(){(div.style.display==`none`)?div.style.display=``:div.style.display=`none`};
 button.addEventListener(`click`,hide);
 document.body.appendChild(button);
-document.body.appendChild(div);// все что выше подготовленно для менюшки
+document.body.appendChild(div);*/// все что выше подготовленно для менюшки
+let div = document.createElement(`div`);
+document.body.appendChild(div);
+div.style.position=`fixed`;
+div.style.width=300+`px`;
+div.style.height=100+`px`;
+let i = document.createElement(`img`);
+i.setAttribute(`src`,`oB.jpg`);
+i.setAttribute(`width`,`100%`);
+i.setAttribute(`height`,`100%`);
+div.appendChild(i);
+
+documet.createElement(table)
